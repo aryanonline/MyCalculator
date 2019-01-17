747361fab94b3c7fda177cb39078a500f31f679a
aryanonline <aryans.www@gmail.com>
2019-01-17 11:08:00 -0500

    Added additional comments

--- src/SimpleCalcGUI.java
+++ src/SimpleCalcGUI.java
@@ -81,8 +81,6 @@ public class SimpleCalcGUI extends JFrame {
             }
         }
 
-        JPanel p1 = new JPanel();
-
 
         JPanel p2 = new JPanel();
         p2.setLayout(new BorderLayout());
@@ -94,11 +92,6 @@ public class SimpleCalcGUI extends JFrame {
         jtaResult.setHorizontalAlignment(JLabel.RIGHT);
         jtaResult.setVerticalAlignment(JLabel.CENTER);
         jtaResult.setPreferredSize(new Dimension(800, 80));
-
-        /*SimpleAttributeSet attrs=new SimpleAttributeSet();
-        StyleConstants.setAlignment(attrs, StyleConstants.ALIGN_RIGHT);
-        StyledDocument doc=(StyledDocument)jtaResult.getDocument();
-        doc.setParagraphAttributes(0,doc.getLength()-1,centre,false);*/
 
         this.getContentPane().add(p2, BorderLayout.NORTH);
         this.getContentPane().add(panel, BorderLayout.CENTER);
@@ -138,6 +131,7 @@ public class SimpleCalcGUI extends JFrame {
         }
     }
 
+    //Creating buttons within calculator
     private void HandleExpression(ActionEvent e){
         JButton btn = (JButton) e.getSource();
         int ItemIndex = (int) btn.getClientProperty("ItemIndex");
@@ -308,6 +302,7 @@ public class SimpleCalcGUI extends JFrame {
                 break;
         }
 
+        //Once the equal button is pressed create the given expression into a text Expression
         if(!lastToken.equals("=")) {
             if (!lastToken.equals(""))
                 tokenArray.push(lastToken);
@@ -315,21 +310,27 @@ public class SimpleCalcGUI extends JFrame {
         }
     }
 
+    //Method to create text expression
     public void createTextExpression(){
+        //initializing text expression
         textExpression = "";
+        //looping through the token array
         for(int i=0; i<tokenArray.size();i++)
+            //setting each text expression as a text expression
             textExpression += tokenArray.get(i);
 
+        //setting the text expression within a jtaResult
         jtaResult.setText(textExpression);
     }
 
+    //adding expression from token array memTokenArray stack. Allows for the expression to be memorised and stored to be used in further evaluations
     public void memorize(){
         memTokenArray.clear();
 
         for(int i=tokenArray.size()-1; i>=0;i--)
             memTokenArray.push(tokenArray.get(i));
     }
-
+    //Recalling the expression that has initially been entered
     public void memRecall(){
         tokenArray.clear();
         textExpression="";
@@ -341,11 +342,11 @@ public class SimpleCalcGUI extends JFrame {
 
         jtaResult.setText(textExpression);
     }
-
+    //clearing the memTokenArray to clear the stored memory
     public void memClear(){
         memTokenArray.clear();
     }
-
+    //Clear everything in calculator
     public void allClear(){
         memTokenArray.clear();
         tokenArray.clear();
@@ -354,9 +355,10 @@ public class SimpleCalcGUI extends JFrame {
         if(is2ndFActive) toggle2NdF();
         jtaResult.setText(textExpression);
     }
-
+    //evaluating the result of the expression
     public void evaluateExpression(){
         try{
+            //text expression is converted into RPN notation and stored within variable exp of type Expression
             Expression exp = new ExpressionBuilder(textExpression).build();
             double d = exp.evaluate();
             jtaResult.setText(String.valueOf(d));
